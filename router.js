ff50aa27fb220be4bf0e1838baa90f8d74e6fff5
Ifechukwu Ejiofor <ejiofori@ensipc94.ensimag.fr>
2024-10-23 18:07:29 +0200

    Version 2.1.0 in progress

--- router.js
+++ router.js
@@ -13,11 +13,47 @@ class Router {
    * 
    */
   sendResponseToClient(requestHandle, responseHandle, resourceRequestedByClient) {
-    var absolutePath = resourceRequestedByClient.substring(1);
-    var controller = this.routingTable[absolutePath];
+    var controllerPath = resourceRequestedByClient.substring(1);
+    
+    var controller = this.routingTable[controllerPath];
+    var controllerParameter = null;
+    
+    if (typeof(controller) != 'function' && controllerPath[controllerPath.length - 1] == '/') {
+      var responseCode = 302;  // This response code is for HTTP "temporary" redirect
+      var responseHeader = { 'Location': controllerPath.substring(0, controllerPath.length - 1) };
+      responseHandle.writeHead(responseCode, responseHeader);
+      responseHandle.end();
+      return;
+    }
+    
+    if (typeof(controller) != 'function' && /^\/categories\/([a-zA-Z0-9-]+)$/.test(controllerPath) == true) {
+      controllerParameter = controllerPath.replace(/\/categories\//g, '').match(/([a-zA-Z0-9-]+)/g);
+      controllerPath = controllerPath.match(/\/categories/g) + '/:categoryId';
+      controller = this.routingTable[controllerPath];
+    }
+    
+    if (typeof(controller) != 'function') {
+      if (/^\/categories\/([a-zA-Z0-9-]+)$/.test(controllerPath) == true) {
+        controllerParameter = controllerPath.replace(/\/categories\//g, '').match(/([a-zA-Z0-9-]+)/g);
+        controllerPath = controllerPath.match(/\/categories/g) + '/:categoryId';
+        controller = this.routingTable[controllerPath];
+      }
+      else if (/^\/change_currency\/([a-zA-Z0-9-]+)$/.test(controllerPath) == true) {
+      console.log(`2, Regular Expression match! controllerPath is ${controllerPath}, controllerParameter is ${controllerParameter}`);
+        controllerParameter = controllerPath.replace(/\/change_currency\//g, '').match(/([a-zA-Z0-9-]+)/g);
+        controllerPath = controllerPath.match(/\/change_currency/g) + '/:currency';
+        controller = this.routingTable[controllerPath];
+      }
+    }
     
     if (typeof(controller) == 'function') {
-      controller(requestHandle, responseHandle);
+      if (controllerParameter == null) {
+        controller(requestHandle, responseHandle);
+      }
+      else {
+      console.log(`2, Regular Expression match! controllerPath is ${controllerPath}, controllerParameter is ${controllerParameter}`);
+        controller(requestHandle, responseHandle, controllerParameter);
+      }
     }
     else {
       sendResponseToClientWithoutRouter(requestHandle, responseHandle, resourceRequestedByClient);
